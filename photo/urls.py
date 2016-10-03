6ad844d68c3b30657049e7770cb7f213f70ca228
Wanseok <wanseok.k@gmail.com>
2016-10-03 18:36:25 +0900

    photoweb modify complete

--- photo/urls.py
+++ photo/urls.py
@@ -14,4 +14,28 @@ urlpatterns = [
 
 	# Example: /photo/99/
 	url(r'^photo/(?P<pk>\d+)/$', PhotoDV.as_view(), name='photo_detail'),
+
+	# Example: /album/add/
+    url(r'^album/add/$', AlbumPhotoCV.as_view(), name='album_add'),
+
+    # Example: /album/change/
+    url(r'^album/change/$', AlbumChangeLV.as_view(), name='album_change'),
+
+    # Example: /album/99/update/
+    url(r'^album/(?P<pk>[0-9]+)/update/$', AlbumPhotoUV.as_view(), name='album_update'),
+
+    # Example: /album/99/delete/
+    url(r'^album/(?P<pk>[0-9]+)/delete/$', AlbumDeleteView.as_view(), name='album_delete'),
+
+    # Example: /photo/add/
+    url(r'^photo/add/$', PhotoCreateView.as_view(), name='photo_add'),
+
+    # Example: /photo/change/
+    url(r'^photo/change/$', PhotoChangeLV.as_view(), name='photo_change'),
+
+    # Example: /photo/99/update/
+    url(r'^photo/(?P<pk>[0-9]+)/update/$', PhotoUpdateView.as_view(), name='photo_update'),
+
+    # Example: /photo/99/delete/
+    url(r'^photo/(?P<pk>[0-9]+)/delete/$', PhotoDeleteView.as_view(), name="photo_delete"),
 ]
